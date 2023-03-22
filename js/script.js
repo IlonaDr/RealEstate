64aafb21bc8ec710f8d30fd15bac1d7bc5c09afa
IlonaDr <112619997+IlonaDr@users.noreply.github.com>
2023-03-22 13:22:37 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.slider-main-block', {
+   loop: true,
+
+   navigation: {
+      nextEl: '.body-main-block__arrow.swiper-button-next',
+      prevEl: '.body-main-block__arrow.swiper-button-prev',
+   },
+
+});
+
+const tabNavItems = document.querySelectorAll('.tabs-deals__button');
+const tabItems = document.querySelectorAll('.item-tabs');
+document.addEventListener("click", function (e) {
+   const targetElement = e.target;
+   let currentActiveIndex = null;
+   let newActiveIndex = null;
+   if (targetElement.closest('.tabs-deals__button')) {
+      tabNavItems.forEach((tabNavItems, index) => {
+         if (tabNavItems.classList.contains('active')) {
+            currentActiveIndex = index;
+            tabNavItems.classList.remove('active');
+         }
+         if (tabNavItems === targetElement) {
+            newActiveIndex = index;
+         }
+      });
+      targetElement.classList.add('active');
+      tabItems[currentActiveIndex].classList.remove('active');
+      tabItems[newActiveIndex].classList.add('active');
+   }
+});
